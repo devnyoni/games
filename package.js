0a6791fe2846fbcb6f26c1d80a76a7203aa032dc
devnyoni <devnyoni@gmail.com>
2026-01-14 02:39:09 +0300

    Update and rename index.html to package.js

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "raheem-bot-deployer",
+  "version": "1.0.0",
+  "description": "Website ya ku-deploy bot kwa Session ID",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "axios": "^1.6.0"
+  }
+}
